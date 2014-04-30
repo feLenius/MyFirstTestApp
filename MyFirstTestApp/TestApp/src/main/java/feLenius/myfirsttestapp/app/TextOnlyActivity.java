6a7075af09f2eae0cdb901ec29836bcc320861c9
linass <linas.sukevicius@gmail.com>
2014-04-30 15:22:57 +0300

    Added new activity, changed settings butten in Action Bar

--- MyFirstTestApp/TestApp/src/main/java/feLenius/myfirsttestapp/app/TextOnlyActivity.java
+++ MyFirstTestApp/TestApp/src/main/java/feLenius/myfirsttestapp/app/TextOnlyActivity.java
@@ -5,19 +5,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public final static String MESSAGE = "feLenius.myfirsttestapp.MESSAGE";
+public class TextOnlyActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MESSAGE);
+
+        setContentView(R.layout.activity_text_only);
+
+        TextView editText = (TextView) findViewById(R.id.text);
+        editText.setText(message);
     }
 
 
@@ -25,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.text_only, menu);
         return true;
     }
 
@@ -36,18 +39,9 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Toast.makeText(getApplicationContext(), "opa opa aaa", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void showText(View view) {
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        Intent intent = new Intent(this, TextOnlyActivity.class);
-        String message = editText.getText().toString();
-        intent.putExtra(MESSAGE, message);
-        startActivity(intent);
     }
 
 }
